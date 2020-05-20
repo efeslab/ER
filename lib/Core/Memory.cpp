8c6cde883a2669f4d5f9c1f6e06807a341702f99
Alkaid <zgf574564920@gmail.com>
2020-05-19 23:38:49 -0400

    Revert "Keep warning about long range array flush due to symbolic read and write"
    This reverts commit 0c81dfba20ffce8f8a390aaa6ac184814ab2b5bb.
    I realize that the warning about flushing long range updates are not
    accurate. It only considers the size of the array being flushed, not the
    actual number of update nodes flushed.

--- lib/Core/Memory.cpp
+++ lib/Core/Memory.cpp
@@ -417,8 +417,9 @@ ref<Expr> ObjectState::read8(ref<Expr> offset) const {
   if (size>4096) {
     std::string allocInfo;
     object->getAllocInfo(allocInfo);
-    klee_warning("flushing %d bytes on read, may be slow and/or crash: %s",
-                 size, allocInfo.c_str());
+    klee_warning_once(0, "flushing %d bytes on read, may be slow and/or crash: %s", 
+                      size,
+                      allocInfo.c_str());
   }
   
   return ReadExpr::create(getUpdates(), ZExtExpr::create(offset, Expr::Int32));
@@ -472,8 +473,9 @@ void ObjectState::write8(ref<Expr> offset, ref<Expr> value,
   if (size>4096) {
     std::string allocInfo;
     object->getAllocInfo(allocInfo);
-    klee_warning("flushing %d bytes on read, may be slow and/or crash: %s",
-                 size, allocInfo.c_str());
+    klee_warning_once(0, "flushing %d bytes on read, may be slow and/or crash: %s", 
+                      size,
+                      allocInfo.c_str());
   }
   
   updates.extend(ZExtExpr::create(offset, Expr::Int32), value, flags, kinst);
